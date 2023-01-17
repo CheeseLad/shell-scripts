fcff4db9a665e0d17727facb3468bf544e4ab3a1
Cheese McCheese <52334071+CheeseLad@users.noreply.github.com>
2023-01-17 23:26:13 +0000

    Add files via upload

---
+++ rpi-plex-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install apt-transport-https -y
+curl https://downloads.plex.tv/plex-keys/PlexSign.key | gpg --dearmor | sudo tee /usr/share/keyrings/plex-archive-keyring.gpg >/dev/null
+echo deb [signed-by=/usr/share/keyrings/plex-archive-keyring.gpg] https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
+sudo apt-get update
+sudo apt install plexmediaserver -y
